8b366d78ed9e0e9556d3bae6a2d719724138514f
Derek Scruggs <me@derekscruggs.com>
2023-11-04 16:05:45 -0500

    Added Threads and Substack icon components

--- src/data/dataDef.ts
+++ src/data/dataDef.ts
@@ -1,7 +1,7 @@
-import {StaticImageData} from 'next/image';
-import {FC, ForwardRefExoticComponent, SVGProps} from 'react';
+import { StaticImageData } from 'next/image';
+import { FC, ForwardRefExoticComponent, SVGProps } from 'react';
 
-import {IconProps} from '../components/Icon/Icon';
+import { IconProps } from '../components/Icon/Icon';
 
 export interface HomepageMeta {
   title: string;
@@ -103,8 +103,10 @@ export interface TestimonialSection {
 
 export interface Testimonial {
   image?: string;
+  link?: string;
   name: string;
   text: string;
+
 }
 
 /**
