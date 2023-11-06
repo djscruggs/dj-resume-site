4ef916938db538d8bb3f82344dcab1ae2952be09
Derek Scruggs <me@derekscruggs.com>
2023-11-05 20:55:33 -0600

    Tweak to portfolio, deployment

--- src/data/dataDef.ts
+++ src/data/dataDef.ts
@@ -1,7 +1,7 @@
-import { StaticImageData } from 'next/image';
-import { FC, ForwardRefExoticComponent, SVGProps } from 'react';
+import {StaticImageData} from 'next/image';
+import {FC, ForwardRefExoticComponent, SVGProps} from 'react';
 
-import { IconProps } from '../components/Icon/Icon';
+import {IconProps} from '../components/Icon/Icon';
 
 export interface HomepageMeta {
   title: string;
@@ -106,7 +106,6 @@ export interface Testimonial {
   link?: string;
   name: string;
   text: string;
-
 }
 
 /**
