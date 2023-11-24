8b589bdbb171fe0ca46659e7210ed0bb50b28b91
Derek Scruggs <me@derekscruggs.com>
2023-11-23 19:27:51 -0600

    removed jest test

--- src/__tests__/index.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from '../pages/index'; // Import the component to test
-
-test('renders homepage', () => {
-  render(<Home />); // Render the component
-
-  // Use Next.js Testing Library queries to assert expected content
-  expect(screen.getByText('Welcome to Next.js!')).toBeInTheDocument();
-});
