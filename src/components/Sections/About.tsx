2385912b2b34695ed92559295dbddfeaafbe65bc
Derek Scruggs <me@derekscruggs.com>
2024-03-01 10:42:34 -0600

    added hashnode link to socials

--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -25,7 +25,7 @@ const About: FC = memo(() => {
             <p className="prose prose-sm text-gray-300 sm:prose-base">{description}</p>
           </div>
           <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-            {aboutItems.map(({label, text, Icon}, idx) => (
+            {aboutItems.map(({ label, text, Icon }, idx) => (
               <li className="col-span-1 flex  items-start gap-x-2" key={idx}>
                 {Icon && <Icon className="h-5 w-5 text-white" />}
                 <span className="text-sm font-bold text-white">{label}:</span>
