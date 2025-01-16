1c298e77a427a7b6e25b68c963acefbccc642483
Derek Scruggs <me@derekscruggs.com>
2025-01-16 10:21:52 -0600

    added rates section

--- src/components/Sections/Header.tsx
+++ src/components/Sections/Header.tsx
@@ -12,7 +12,7 @@ export const headerID = 'headerNav';
 const Header: FC = memo(() => {
   const [currentSection, setCurrentSection] = useState<SectionId | null>(null);
   const navSections = useMemo(
-    () => [SectionId.About, SectionId.Work, SectionId.Portfolio, SectionId.Testimonials, SectionId.Contact],
+    () => [SectionId.About, SectionId.Work, SectionId.Portfolio, SectionId.Testimonials, SectionId.Rates, SectionId.Contact],
     [],
   );
 
